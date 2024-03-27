5991b5f7ebb86fdb09c536145b1d2791b8f66b6a
Tej-Mouse <tjman441@gmail.com>
2024-03-26 21:09:46 -0400

    Man, You Don't Even Want To Know

---
+++ The_Dungeon_of_the_Dragon/Scripts/style.py
@@ -0,0 +1,50 @@
+from PyQt5.QtGui import QFont
+
+LabelFont1 = QFont('Times', 15, QFont.Decorative)
+
+LabelFont2 = QFont('Times', 12, QFont.Decorative)  # Attributes
+LabelFont3 = QFont('Times', 10, QFont.Decorative)  # Skills
+
+ItemLabelFont = QFont('Times', 14, QFont.Decorative)
+
+# Colors
+main_highlight = "darkred"
+secondary_higlight = "crimson"
+
+# StyleSheets_______________________________________________________________________________________
+TabButtonSheet = "QPushButton {color: white}"
+TabButtonSheet1 = ("QPushButton {background-color: #3e3e42;color: white;  border-radius: 15px}"
+                   "QPushButton:pressed {color: white; background-color: darkgrey}"
+                   "QPushButton:hover {color: white; background-color: darkred}"
+                   )
+TabButtonSheet2 = ("QPushButton {color: white}"
+                   "QPushButton:pressed {color: white; background-color: darkgrey}"
+                   "QPushButton:hover {color: white; background-color: darkred}"
+                   )
+SubButtonSheet = ("QPushButton {color: white; background-color: grey; border-radius: 9px} "
+                  "QPushButton:pressed {color: white; background-color: darkgrey}"
+                  "QPushButton:hover {color: white; background-color: " + main_highlight + "}"
+                  )
+
+SubCheckedSheet = ("QCheckBox {color: white; background-color: grey; border-radius: 9px} "
+                   "QCheckBox:pressed {color: white; background-color: darkgrey}"
+                   "QCheckBox:hover {color: white; background-color: darkred}"
+                   )
+
+DarkGreyLabel = "QLabel {background-color: #a2a2a2;color: black}"
+GreyLabel = ("QLabel {background-color: #3e3e42;color: white;  border-radius: 15"
+             "px}")
+
+ItemFrame = ("QFrame {border-radius: 15px}"
+             "QFrame:hover {color: white; background-color: " + main_highlight + "}"
+             )
+ItemInnerLabel = ("QLabel {background-color: #3e3e42;color: white;  border-radius: 15px}"
+                  "QLabel:pressed {color: white; background-color: darkgrey}"
+                  "QLabel:hover {color: white; background-color: " + secondary_higlight + "}"
+                  )
+
+
+
+#  Encumberance Style__________________________________________________________________
+
+
